0cd366e889b942613c064f2ab02d344274af4183
Sayali Jadhav1 <sayali.jadhav1@ibm.com>
2023-05-05 15:07:54 +0530

    GFG Delete node from a linked list without reference to Head node

---
+++ DeleteNodeWithoutHeadReference.java
@@ -0,0 +1,20 @@
+class Node
+{
+	int data ;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+
+//Function to delete a node without any reference to head pointer.
+public class DeleteNodeWithoutHeadReference
+{
+    void deleteNode(Node del)
+    {
+         del.data = del.next.data;
+         del.next = del.next.next;
+    }
+}
